f15ec880330095ba5fd15fc643c05e631b3989c9
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-10-18 15:27:02 -0300

    Uso de tags ?: para informar o não uso do grupo em memória

--- grupos_retrovisores.py
+++ grupos_retrovisores.py
@@ -9,10 +9,12 @@ texto = '''
 print(re.findall(r'<([pdiv]{1,3})>.*?<\/\1>', texto))
 print(re.findall(r'(<([pdiv]{1,3})>.*?<\/\2>)', texto))
 print(re.findall(r'<([pdiv]{1,3})>(.*?)<\/\1>', texto))
-tags = re.findall(r'(<([pdiv]{1,3})>(.*?)<\/\2>)', texto)
+# tags = re.findall(r'<([pdiv]{1,3})>(.*?)<\/\1>', texto)
+tags = re.findall(r'<([pdiv]{1,3})>(?:.*?)<\/\1>', texto)
+print(tags)
 
-for t in tags:
-    um, dois, tres = t
-    # if tres == '':
-    #     tres = 'vazio'
-    print(tres)
+# for t in tags:
+#     um, dois, tres = t
+#     # if tres == '':
+#     #     tres = 'vazio'
+#     print(tres)
